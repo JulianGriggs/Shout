4ad0a52cde7bb25b959d58b5032fbacdbc53257f
JulianGriggs <jgriggs@princeton.edu>
2014-03-25 01:56:13 -0400

    Built up a very basic table view with sample data
    I’m hoping that you’ll be able to load the Xcode project the correct way

---
+++ Shout/Shout/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Shout
+//
+//  Created by Julian Griggs on 3/25/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shout/Shout/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  Shout
+//
+//  Created by Julian Griggs on 3/25/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController <UITableViewDataSource>
+
+@end
