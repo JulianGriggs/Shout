c65843c864bf670ca6ad5be89dba95f7b9ba775a
JulianGriggs <jgriggs@princeton.edu>
2014-03-28 20:35:37 -0400

    Got Feed to load JSON Data

---
+++ Shout/Shout/JCCFeedTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  JCCFeedTableViewController.h
+//  Shout
+//
+//  Created by Julian Griggs on 3/28/14.
+//  Copyright (c) 2014 Shout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCCFeedTableViewController : UITableViewController
+{
+    NSArray *shouts;
+}
+
+- (void)fetchShouts;
+
+@end
