c21cfdf045a3787f9b107f38e230d0c64ec6aea8
cbporter <cameronporter@nat-oitwireless-inside-vapornet100-c-15848.princeton.edu>
2014-03-28 23:00:33 -0400

    Camerons Post UI
    yep

---
+++ Shout/Shout/JCCPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCCPostViewController.h
+//  Shout
+//
+//  Created by Cameron Porter on 3/28/14.
+//  Copyright (c) 2014 Shout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCCPostViewController : UIViewController <UITextViewDelegate>
+
+@end
