9f2368b3518bbe12b806f7db2186d5ab0bfc5550
JulianGriggs <jgriggs@princeton.edu>
2014-03-28 20:11:10 -0400

    Fresh Start for Shout Interface
    It is called Shout

---
+++ Shout/Shout/JCCViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCCViewController.h
+//  Shout
+//
+//  Created by Julian Griggs on 3/28/14.
+//  Copyright (c) 2014 Shout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCCViewController : UIViewController
+
+@end
---
+++ Shout/Shout/JCCViewController.m
@@ -0,0 +1,29 @@
+//
+//  JCCViewController.m
+//  Shout
+//
+//  Created by Julian Griggs on 3/28/14.
+//  Copyright (c) 2014 Shout. All rights reserved.
+//
+
+#import "JCCViewController.h"
+
+@interface JCCViewController ()
+
+@end
+
+@implementation JCCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
