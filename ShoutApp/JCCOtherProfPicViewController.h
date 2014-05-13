22da40a1a61e4eb5b1082fa50f84f8d9646c7543
JulianGriggs <jgriggs@princeton.edu>
2014-05-13 00:05:40 -0400

    Can look at someone else's profile picture

---
+++ ShoutApp/JCCOtherProfPicViewController.h
@@ -0,0 +1,14 @@
+//
+//  JCCOtherProfPicViewController.h
+//  ShoutApp
+//
+//  Created by Julian Griggs on 5/12/14.
+//  Copyright (c) 2014 Shout. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCCOtherProfPicViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) UIImage *profPicture;
+@end
---
+++ ShoutApp/JCCOtherProfPicViewController.m
@@ -0,0 +1,41 @@
+//
+//  JCCOtherProfPicViewController.m
+//  ShoutApp
+//
+//  Created by Julian Griggs on 5/12/14.
+//  Copyright (c) 2014 Shout. All rights reserved.
+//
+
+#import "JCCOtherProfPicViewController.h"
+
+@interface JCCOtherProfPicViewController ()
+
+@end
+
+@implementation JCCOtherProfPicViewController
+{
+    UIImage *newProfImage;
+}
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    [self.imageView setImage:self.profPicture];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
